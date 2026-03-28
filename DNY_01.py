d89f569cfcb4eb3a750876f35be9f2d5712b1047
pradeepkalmodiya5563 <pradeep.kalmodiya@avantika.edu.in>
2026-03-28 14:47:36 +0000

    start

--- DNY_01.py
+++ DNY_01.py
@@ -7,3 +7,5 @@ def calculate_fine(book_title, days_overdue, daily_rate=5.0, max_fine=150.0):
         max_msg = False
 
     return fine, max_msg
+    # -------- MAIN PROGRAM --------
+data = input().split()
